5dc2374a69496fb684271db68d16cacbc23c97a4
paul-tes <pauasca8462@gmail.com>
2023-11-02 18:53:24 +0300

    Bottom menue driven app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:todo/pages/first_page.dart';
+import 'package:todo/pages/home_page.dart';
+import 'package:todo/pages/settings_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void tapMe() {
+    print("Auuuche!");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: FristPage(),
+      routes: {
+        'firstPage': (context) => FristPage(),
+        'homepage': (context) => Home(),
+        'settingspage': (context) => Settings(),
+      },
     );
   }
 }
